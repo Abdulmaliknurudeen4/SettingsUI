40aee64a25ec8641acf48dbc443501c72da58e77
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2021-11-21 23:01:06 +0100

    Add UserProfile UI/Controller Code

--- src/main/java/com/example/undecoratedscene/UserProfileController.java
+++ src/main/java/com/example/undecoratedscene/UserProfileController.java
@@ -1,4 +1,11 @@
 package com.example.undecoratedscene;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class UserProfileController {
+    public void initialize(URL url, ResourceBundle rb){
+//        makeStageDraggable();
+
+    }
 }
